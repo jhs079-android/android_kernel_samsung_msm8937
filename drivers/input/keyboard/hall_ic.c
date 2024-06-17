LineageOS/android_kernel_samsung_sdm670

input: keyboard: hall_ic: Report SW_LID instead of SW_FLIP
* Android expects SW_LID for lid events.

Change-Id: Iebffbabdbb3748eec4f887ebd227c67adf01d8ef
Signed-off-by: Roman Birg <roman@cyngn.com>

drivers/input/keyboard/hall_ic.c
@@ -77,7 +77,7 @@ static void flip_cover_work(struct work_struct *work)
 
 	if (first == second) {
 		sec_flip_cover = first;
-		input_report_switch(ddata->input, SW_FLIP, sec_flip_cover);
+		input_report_switch(ddata->input, SW_LID, sec_flip_cover);
 		input_sync(ddata->input);
 	}
 }
@@ -97,7 +97,7 @@ static void flip_cover_work(struct work_struct *work)
 
 	sec_flip_cover = first;
 	input_report_switch(ddata->input,
-			SW_FLIP, ddata->flip_cover);
+			SW_LID, ddata->flip_cover);
 	input_sync(ddata->input);
 #else
 	ddata->flip_cover = !gpio_get_value(ddata->gpio_flip_cover_key1) & !gpio_get_value(ddata->gpio_flip_cover_key2);
@@ -109,7 +109,7 @@ static void flip_cover_work(struct work_struct *work)
 			ddata->emulated_hall_ic_status = !ddata->emulated_hall_ic_status;
 			pr_info("keys:%s #1 : %d , %d\n", __func__, first, ddata->emulated_hall_ic_status);	
 			input_report_switch(ddata->input,
-					SW_FLIP, ddata->emulated_hall_ic_status);
+					SW_LID, ddata->emulated_hall_ic_status);
 			input_sync(ddata->input);
 		}
 		sec_flip_cover = first;
@@ -278,7 +278,7 @@ static int hall_probe(struct platform_device *pdev)
 	input->dev.parent = &pdev->dev;
 
 	input->evbit[0] |= BIT_MASK(EV_SW);
-	input_set_capability(input, EV_SW, SW_FLIP);
+	input_set_capability(input, EV_SW, SW_LID);
 
 	input->open = hall_open;
 	input->close = hall_close;

jhs079-android/android_kernel_samsung_msm8937

drivers/input/keyboard/hall_ic.c
@@ -71,7 +71,7 @@ static void flip_cover_work(struct work_struct *work)
 #if defined(CONFIG_SENSORS_HALL_FOLDER)
 		input_report_switch(ddata->input, SW_LID, !flip_cover);	/* foder open : 0, close : 1 */
 #else
-		input_report_switch(ddata->input, SW_FLIP, flip_cover);
+		input_report_switch(ddata->input, SW_LID, flip_cover);
 #endif
 		input_sync(ddata->input);
 	}
@@ -92,7 +92,7 @@ static void flip_cover_work(struct work_struct *work)
 #if defined(CONFIG_SENSORS_HALL_FOLDER)
 	input_report_switch(ddata->input, SW_LID, !flip_cover);	/* foder open : 0, close : 1 */
 #else
-	input_report_switch(ddata->input, SW_FLIP, flip_cover);
+	input_report_switch(ddata->input, SW_LID, flip_cover);
 #endif
 	input_sync(ddata->input);
 }
@@ -243,7 +243,7 @@ static int hall_probe(struct platform_device *pdev)
 #if defined(CONFIG_SENSORS_HALL_FOLDER)
 	input_set_capability(input, EV_SW, SW_LID);
 #else
-	input_set_capability(input, EV_SW, SW_FLIP);
+	input_set_capability(input, EV_SW, SW_LID);
 #endif
 
 	input->open = hall_open;